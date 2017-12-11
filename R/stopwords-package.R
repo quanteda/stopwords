94bccf2444bc1250ead6911ba5b46b4338f5becb
Kenneth Benoit <kbenoit@lse.ac.uk>
2017-12-11 16:58:37 +0000

    Update package documentation

--- R/stopwords-package.R
+++ R/stopwords-package.R
@@ -6,12 +6,18 @@
 #'
 #' @section Currently available sources:
 #' \describe{
-#' \item{\code{snowball}}{The (mostly) SnowBall sources for multiple languages,
-#' with a few added languages to ensure compatibility with the \pkg{quanteda}
+#' \item{\code{\link[=data_stopwords_snowball]{snowball}}}{The Snowball stopword lists sources for multiple languages.
+#' Most of these have been ported from the \pkg{quanteda}
 #' stopword lists (in versions <1.0 of that package).}
 #'
-#'  \item{\code{stopwords-iso}}{The collection taken from
+#'  \item{\code{\link[=data_stopwords_stopwordsiso]{stopwords-iso}}}{The collection taken from
 #'  \url{https://github.com/stopwords-iso/stopwords-iso/}.}
+#'
+#'  \item{\code{\link[=data_stopwords_smart]{smart}}}{The English-language stopword list from the SMART information
+#'  retrieval system.}
+#'
+#'  \item{\code{\link[=data_stopwords_misc]{misc}}}{A few additional stopword lists, including the non-Snowball
+#'  word lists from \pkg{quanteda} versions < 1.0.}
 #'  }
 #' @author Kenneth Benoit, David Muhr, and Kohei Watanabe
 #' @docType package
