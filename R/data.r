9344cd83621a199b07ac7ee7ceaeffffe093453d
Kenneth Benoit <kbenoit@lse.ac.uk>
2017-12-11 16:58:01 +0000

    Update data objects
    - separate and expand snowball
    - move non-snowball to misc
    - move SMART to smart
    - improve documentation
    - add references

--- R/data.r
+++ R/data.r
@@ -2,113 +2,82 @@
 #'
 #' The Stopwords ISO Dataset is the most comprehensive collection of stopwords
 #' for multiple languages. The collection follows the ISO 639-1 language code.
-#'
-#' @format A list of character vectors that represent stopwords:
-#' \describe{
-#'   \item{af}{Afrikaans}
-#'   \item{ar}{Arabic}
-#'   \item{hy}{Armenian}
-#'   \item{eu}{Basque}
-#'   \item{bn}{Bengali}
-#'   \item{br}{Breton}
-#'   \item{bg}{Bulgarian}
-#'   \item{ca}{Catalan}
-#'   \item{zh}{Chinese}
-#'   \item{hr}{Croatian}
-#'   \item{cs}{Czech}
-#'   \item{da}{Danish}
-#'   \item{nl}{Dutch}
-#'   \item{en}{English}
-#'   \item{eo}{Esperanto}
-#'   \item{et}{Estonian}
-#'   \item{fi}{Finnish}
-#'   \item{fr}{French}
-#'   \item{gl}{Galician}
-#'   \item{de}{German}
-#'   \item{el}{Greek}
-#'   \item{ha}{Hausa}
-#'   \item{he}{Hebrew}
-#'   \item{hi}{Hindi}
-#'   \item{hu}{Hungarian}
-#'   \item{id}{Indonesian}
-#'   \item{ga}{Irish}
-#'   \item{it}{Italian}
-#'   \item{ja}{Japanese}
-#'   \item{ko}{Korean}
-#'   \item{ku}{Kurdish}
-#'   \item{la}{Latin}
-#'   \item{lt}{Lithuanian}
-#'   \item{lv}{Latvian}
-#'   \item{ms}{Malay}
-#'   \item{mr}{Marathi}
-#'   \item{no}{Norwegian}
-#'   \item{fa}{Persian}
-#'   \item{pl}{Polish}
-#'   \item{pt}{Portuguese}
-#'   \item{ro}{Romanian}
-#'   \item{ru}{Russian}
-#'   \item{sk}{Slovak}
-#'   \item{sl}{Slovenian}
-#'   \item{so}{Somali}
-#'   \item{st}{Southern Sotho}
-#'   \item{es}{Spanish}
-#'   \item{sw}{Swahili}
-#'   \item{sv}{Swedish}
-#'   \item{th}{Thai}
-#'   \item{tl}{Tagalog}
-#'   \item{tr}{Turkish}
-#'   \item{uk}{Ukrainian}
-#'   \item{ur}{Urdu}
-#'   \item{vi}{Vietnamese}
-#'   \item{yo}{Yoruba}
-#'   \item{zu}{Zulu}
-#' }
+#' @usage NULL
+#' @section Usage:
+#' \code{stopwords(language, source = "stopwords-iso")}
+#' @format A named list of length 57, of character vectors that represent
+#'   stopwords in 57 languages.  To see the languages available, use
+#'   \code{\link{stopwords_getlanguages}}.
 #' @source \url{https://github.com/stopwords-iso/stopwords-iso/}
 "data_stopwords_stopwordsiso"
 
 #' Internal function to update the data file
+#'
+#' Fetches new data from remote sources, which can be used to regenerate the stopword files.
 #' @keywords internal
 #' @examples
-#' \dontrun{
-#' stopwords:::update_stopwords()
-#' }
-update_stopwords <- function() {
-
+#' # data_stopwords_stopwordsiso <- update_stopwords("stopwords-iso)"
+#' # devtools::use_data(data_stopwords_stopwordsiso)
+update_stopwords <- function(source = c("stopwords-iso")) {
+  source <- match.arg(source)
   url <- 'https://raw.githubusercontent.com/stopwords-iso/stopwords-iso/master/stopwords-iso.json'
+  message("Rebuilding data from https://github.com/stopwords-iso/stopwords-iso/blob/master/stopwords-iso.json")
   json <- paste0(readLines(url, warn = FALSE), collapse="")
-  data_char_stopwords_iso <- rjson::fromJSON(json)
-  cat('Downloaded stopwords for', length(data), 'languages.\n')
-  path <- 'data/data_char_stopwords_iso.RData'
-  save(data_char_stopwords_iso, file = path)
-  cat('Saved to', path, '.\n')
+  message("data_stopwords_stopwordsiso has been updated.\n")
+  message("To save:\n data_stopwords_stopwordsiso <- update_stopwords(\"stopwords-iso)\"\n devtools::use_data(data_stopwords_stopwordsiso)")
+  invisible(rjson::fromJSON(json))
 }
 
 
 #' snowball stopword list
 #'
-#' Ported from versions < 1.0 of the\pkg{quanteda} package.
-#' @details \code{data_char_stopwords} provides stopword lists in multiple
-#'   languages; it is a named list of characters with the lowercase language
-#'   name (in English) as the name of each list element.
-#'   Supported languages are Arabic, Danish, Dutch, English, Finnish, French,
-#'   German, Greek, Hungarian, Italian, Norwegian, Portuguese, Russian, Spanish,
-#'   and Swedish.  For compatibility with the names used in the \pkg{quanteda}
-#'   package from which this source was first adapted, we have retained the
-#'   "SMART", "Greek", and "Chinese" stopword lists.
+#' @usage NULL
+#' @section Usage:
+#' \code{stopwords(language, source = "snowball")}
+#' @details Provides stopword lists in multiple
+#'   languages, based on the Snowball stemmer's word lists.
 #' @source
 #'   The main stopword lists are taken from the Snowball stemmer project in
 #'   different languages (see \url{http://snowballstem.org/projects.html}).
 #'
-#'   The Greek stopwords were supplied by Carsten Schwemmer (see
-#'   \href{https://github.com/kbenoit/quanteda/issues/282}{GitHub issue #282}).
-#'
-#'   The Chinese stopwords are taken from the
-#'   \href{http://www.baiduguide.com/baidu-stopwords/}{Baidu stopword list}.
-#'
-#'   The English "SMART" stopwords are taken from the SMART information retrieval
-#'   system (obtained from Lewis, David D., et al.
-#'   "\href{http://www.jmlr.org/papers/volume5/lewis04a/lewis04a.pdf}{Rcv1: A
-#'   new benchmark collection for text categorization research.}" \emph{Journal
-#'   of machine learning research} (2004, 5 April): 361-397.
+#'   The stopword lists can be found in \url{http://snowball.tartarus.org/dist/snowball_all.tgz}.
 #' @seealso \code{\link{stopwords}}
 "data_stopwords_snowball"
+
+
+#' miscellaneous stopword lists
+#'
+#' Other, miscellaneous stopword lists.
+#' @usage NULL
+#' @section Usage:
+#' \code{stopwords(language, source = "misc")}
+#' @source
+#'   The Arabic stopwords were supplied by Alexandra Siegel (documentation pending).
+#'
+#'   The Catalan stopwords come from \url{http://latel.upf.edu/morgana/altres/pub/ca_stop.htm}.
+#'
+#'   The Greek stopwords were supplied by Carsten Schwemmer (see
+#'   \url{https://github.com/kbenoit/quanteda/issues/282}).
+#'
+#'   The Chinese stopwords are taken from the
+#'   \url{http://www.baiduguide.com/baidu-stopwords/}{Baidu stopword list}.
+"data_stopwords_misc"
+
+#' stopword lists from the SMART system
+#'
+#' The stopword lists based on the SMART (System for the Mechanical Analysis and
+#' Retrieval of Text) Information Retrieval System, an information retrieval
+#' system developed at Cornell University in the 1960s.
+#' @usage NULL
+#' @section Usage:
+#' \code{stopwords(language = "en", source = "smart")}
+#' @source
+#'   The English stopwrod list is taken from the
+#'   \href{http://www.ai.mit.edu/projects/jmlr/papers/volume5/lewis04a/a11-smart-stop-list/english.stop}{online appendix 11}
+#'   of Lewis et. al. (2004).
+#' @references
+#'   Lewis, David D., et al. (2004)
+#'   "\href{http://www.jmlr.org/papers/volume5/lewis04a/lewis04a.pdf}{Rcv1: A
+#'   new benchmark collection for text categorization research.}" \emph{Journal
+#'   of machine learning research} 5: 361-397.
+"data_stopwords_smart"
