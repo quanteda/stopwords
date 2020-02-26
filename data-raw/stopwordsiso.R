e937b67a8d29fc6870fca23942c2ea45826366e3
kbenoit <kbenoit@lse.ac.uk>
2020-02-26 15:09:55 +1100

    add code to remove duplicates

--- data-raw/stopwordsiso.R
+++ data-raw/stopwordsiso.R
@@ -5,5 +5,8 @@ library(jsonlite)
 # Convert JSON to R data structure
 data_stopwords_stopwordsiso <- read_json("data-raw/stopwords-iso/stopwords-iso.json", simplifyVector = TRUE)
 
+# remove duplicates
+data_stopwords_stopwordsiso <- lapply(data_stopwords_stopwordsiso, unique)
+
 # Save `data_stopwords_stopwordsiso` to data directory
 devtools::use_data(data_stopwords_stopwordsiso, overwrite = TRUE)
