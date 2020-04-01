85d9f9704569d0237ada38d9fd7fabd1b261a980
Kohei Watanabe <watanabe.kohei@gmail.com>
2020-04-01 08:20:44 +0200

    Add Chinese

--- data-raw/stopwords_marimo.R
+++ data-raw/stopwords_marimo.R
@@ -1,13 +1,25 @@
 library("yaml")
 library("magrittr")
 
-files <- list.files(path = "marimo", full.names = TRUE)
-langs <- stringi::stri_extract_last_regex(files, "\\w{2}\\.yml$") %>%
-  stringi::stri_sub(to = -5)
+merge_unnamed <- function(x) {
+  if (is.null(names(x))) {
+    return(unlist(x, use.names = FALSE) )
+  } else {
+    lapply(x, merge_unnamed)
+  }
+}
+
+files <- c("en" = "stopwords_en.yml",
+           "ja" = "stopwords_ja.yml",
+           "ar" = "stopwords_ar.yml",
+           "je" = "stopwords_he.yml",
+           "zh_tw" = "stopwords_zh_traditional.yml",
+           "zh_cn" = "stopwords_zh_simplified.yml")
 
 data_stopwords_marimo <- list()
-for (i in seq_along(files)) {
-  data_stopwords_marimo[[langs[i]]] <- read_yaml(files[i])
+for (lang in names(files)) {
+  lis <- read_yaml(paste0("data-raw/marimo/", files[lang]))
+  data_stopwords_marimo[[lang]] <- lapply(lis, merge_unnamed)
 }
 
 # Save `data_stopwords_stopwordsiso` to data directory
